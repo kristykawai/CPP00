7325e5144f435773b2167408cf8fa85c5289d969
kchan <kristy.kw.chan@gmail.com>
2024-03-22 17:54:12 +1030

    testing ex01

---
+++ ex01/Contact.hpp
@@ -0,0 +1,17 @@
+# ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+#include <iostream>
+#include <string>
+
+class Contact
+{
+	private:
+		std::string firstname;
+		std::string lastname;
+		std::string title;
+		std::string email;
+		std::string number;
+		int index;
+};
+#endif
